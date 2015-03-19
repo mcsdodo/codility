6fb2bc0eb070f8ea48905e2309fb67fd436f8615
Lačný Jozef <Jozef.Lacny@steinringer.sk>
2015-03-19 21:16:46 +0100

    first 2

---
+++ FrogJmp.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility
+{
+	[TestClass]
+	public class FrogJmp
+	{
+		int solution(int X, int Y, int D)
+		{
+			if ((Y - X) < D && Y != X) return 1;
+			var mod = (Y - X) % D;
+			var result = (double)((Y - X) + mod) / D;
+			
+			return (int)Math.Ceiling(result);
+		}
+
+		[TestMethod]
+		public void Test10()
+		{
+			//Arrange
+			var X = 1;
+			var Y = 21;
+			var D = 10;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(2, solution(X, Y, D));
+		}
+
+		[TestMethod]
+		public void Test9()
+		{
+			//Arrange
+			var X = 1;
+			var Y = 12;
+			var D = 10;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(2, solution(X, Y, D));
+		}
+
+		[TestMethod]
+		public void Test8()
+		{
+			//Arrange
+			var X = 1;
+			var Y = 10;
+			var D = 10;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(1, solution(X, Y, D));
+		}
+
+		[TestMethod]
+		public void Test7()
+		{
+			//Arrange
+			var X = 1;
+			var Y = 2;
+			var D = 100000;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(1, solution(X, Y, D));
+		}
+
+		[TestMethod]
+		public void Test6()
+		{
+			//Arrange
+			var X = 1;
+			var Y = 1;
+			var D = 100000;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(0, solution(X, Y, D));
+		}
+
+		[TestMethod]
+		public void Test5()
+		{
+			//Arrange
+			var X = 1;
+			var Y = 1;
+			var D = 1;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(0, solution(X, Y, D));
+		}
+
+		[TestMethod]
+		public void Test4()
+		{
+			//Arrange
+			var X = 1;
+			var Y = 1000000000;
+			var D = 1;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(999999999, solution(X, Y, D));
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			//Arrange
+			var X = 10;
+			var Y = 85;
+			var D = 30;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(3, solution(X, Y, D));
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			//Arrange
+			var X = 1;
+			var Y = 3;
+			var D = 1;
+
+			//Act
+
+
+			//Assert
+			Assert.AreEqual(2, solution(X, Y, D));
+		}
+
+
+		[TestMethod]
+		public void Test1()
+		{
+			var X = 1;
+			var Y = 2;
+			var D = 1;
+
+			Assert.AreEqual(1, solution(X,Y,D));
+		}
+	}
+}
---
+++ PermMissingElem.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility
+{
+	[TestClass]
+	public class PermMissingElem
+	{
+		public int solution(int[] A)
+		{
+			double sum = 0;
+			double length = (double)A.Length;
+
+			for (int i = 0; i < length; i++)
+			{
+				sum += A[i];
+			}
+
+			var supposedSum = (length + 1) * ((double)(length + 2) / 2);
+
+			return (int)(supposedSum - sum);
+		}
+
+		int[] GetArray(int length, int missing)
+		{
+			List<int> list = new List<int>();
+			for (int i = 1; i <= length; i++)
+			{
+				if (i != missing) list.Add(i);
+			}
+			list.Reverse();
+			return list.ToArray();
+		}
+
+		[TestMethod]
+		public void Test7()
+		{
+			//Arrange
+			var arr = GetArray(1000000, 5);
+
+			//Act
+			var res = solution(arr);
+
+			//Assert
+			Assert.AreEqual(5, res);
+
+		}
+
+		[TestMethod]
+		public void Test6()
+		{
+			//Arrange
+			var arr = GetArray(100000, 99999);
+
+			//Act
+			var res = solution(arr);
+
+			//Assert
+			Assert.AreEqual(99999, res);
+
+		}
+
+		[TestMethod]
+		public void Test5()
+		{
+			//Arrange
+			var arr = GetArray(5000, 5);
+
+			//Act
+			var res = solution(arr);
+
+			//Assert
+			Assert.AreEqual(5, res);
+		}
+
+		[TestMethod]
+		public void Test4()
+		{
+			//Arrange
+			var arr = GetArray(10,4);
+			
+			//Act
+			var res = solution(arr);
+
+			//Assert
+			Assert.AreEqual(4, res);
+
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			//Arrange
+			var arr = GetArray(10, 9);
+
+			//Act
+			var res = solution(arr);
+
+			//Assert
+			Assert.AreEqual(9, res);
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			//Arrange
+			var arr = new [] {1,3};
+
+			//Act
+			var res = solution(arr);
+
+			//Assert
+			Assert.AreEqual(2,res);
+		}
+
+		[TestMethod]
+		public void Test1() 
+		{
+			//Arrange
+			var arr = new [] {2, 3, 1, 5};
+
+			//Act
+			var res = solution(arr);
+
+			//Assert
+			Assert.AreEqual(4,res);
+		}
+	}
+}
